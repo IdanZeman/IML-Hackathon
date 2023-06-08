8048caa8eba63c6e99cdedc62d7d3799cffcc8d0
zemani2 <idan.zeman@mail.huji.ac.il>
2023-06-08 13:21:02 +0300

    main

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-
+import numpy as np
 from preprocess import preprocess
 
 
 def soft_svm_model():
     # Assuming you have your features stored in X and labels in y
     X, y = preprocess(filename="Data/agoda_cancellation_train.csv")
-    ###
-    c = ['hotel_live_date', 'accommadation_type_name', 'original_payment_method',
-               'original_payment_type', 'is_user_logged_in', 'cancellation_policy_code',
-               'is_first_booking', 'delta_cancel_before_checking', 'charge_option']
-    X = X.drop(columns=c)
-    y = y.drop(columns=c)
-    ###
+    # ###
+    # c = ['hotel_live_date', 'accommadation_type_name', 'original_payment_method',
+    #            'original_payment_type', 'is_user_logged_in', 'cancellation_policy_code',
+    #            'is_first_booking', 'delta_cancel_before_checking', 'charge_option']
+    # X = X.drop(columns=c)
+    # y = y.drop(columns=c)
+    # ###
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.999,
                                                         random_state=42)  # TODO: check random_state
-    svm = SVC(kernel='linear')
-
-    svm.fit(X_train.iloc[:, :2], y_train)
+    svm = SVC(kernel='linear', probability=True)
+    x = np.random.uniform(low=-5, high=5, size=(10, 2))
+    X, y = (x, np.array(x[:,0] < x[:,1], dtype=np.int))
+    svm.fit(X, y)
     print("finish fit")
     y_pred = svm.predict(X_test)
     print(y_pred)
