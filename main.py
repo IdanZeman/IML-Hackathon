4f549346809a52f8bee4178f2de9f761ad3c582b
zemani2 <idan.zeman@mail.huji.ac.il>
2023-06-08 19:05:51 +0300

    main

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
@@ -26,23 +28,49 @@ def soft_svm_model():
         KNeighborsClassifier(n_neighbors=5),
         # SVC(kernel='linear', probability=True),
         LinearDiscriminantAnalysis(store_covariance=True),
-        QuadraticDiscriminantAnalysis(store_covariance=True)
+        QuadraticDiscriminantAnalysis(store_covariance=True),
+        RandomForestClassifier(max_depth=5,random_state=0)
+
     ]
     ## "Linear SVM"
-    model_names = ["Logistic regression", "Desicion Tree (Depth 5)", "KNN", "LDA", "QDA"]
+    model_names = ["Logistic regression", "Desicion Tree (Depth 5)", "KNN", "LDA", "QDA","Random Forest Classifier"]
     df = pd.read_csv("Data/agoda_cancellation_train.csv")
     X, y = df.drop(columns=["cancellation_datetime"]), df["cancellation_datetime"]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25,
                                                         random_state=42)  # TODO: check random_state
-    preprocessed_X_train, preprocessed_y_train = preprocess_train(X_train, y_train)
-    for i, model in enumerate(models):
-        model.fit(X_train.drop(columns=["h_booking_id"]).iloc[:, 1:].to_numpy(), y_train.to_numpy())
-        print("Model - " + model_names[i]+": ")
-        y_pred = model.predict(X_test.drop(columns=["h_booking_id"]).iloc[:, 1:].to_numpy())
-        print(y_pred)
-        accuracy = accuracy_score(y_test, y_pred)
-        print("Accuracy:", accuracy)
+    y_test = y_test.notnull().astype(int)
 
+    (preprocessed_X_train, preprocessed_y_train), (preprocessed_X_test, preprocessed_y_test) =\
+        preprocess(X_train.drop(columns=["h_booking_id"]), y_train), preprocess(X_test.drop(columns=["h_booking_id"]), None)
+    all_dummis = set(preprocessed_X_train.columns).union(preprocessed_X_test.columns)
+    preprocessed_X_train = preprocessed_X_train.reindex(columns=all_dummis, fill_value=0)
+    preprocessed_X_test = preprocessed_X_test.reindex(columns=all_dummis, fill_value=0)
+
+    X_agoda_test = pd.read_csv("Data/Agoda_Test_1.csv")
+    preprocessed_X_agoda_test, _ = preprocess(X_agoda_test.drop(columns=["h_booking_id"]), None)
+    preprocessed_X_agoda_test = preprocessed_X_agoda_test.reindex(columns=all_dummis, fill_value=0)
+    y_pred_arr = []
+    accuracy_scores = []
+    for i, model in enumerate(models):
+        model.fit(preprocessed_X_train.to_numpy(), preprocessed_y_train.to_numpy())
+        print("Model - " + model_names[i]+": ")
+        y_pred = model.predict(preprocessed_X_test.to_numpy())
+        y_pred_arr.append(y_pred)
+        accuracy = accuracy_score(y_test.loc[preprocessed_X_test.index], y_pred)
+        Tp = np.sum[y_pred == y_test.loc[preprocessed_X_test.index]]
+        Fp = np.sum[y_pred != y_test.loc[preprocessed_X_test.index] and y_pred == 1]
+        Fn = np.sum[y_pred != y_test.loc[preprocessed_X_test.index] and y_pred == 0]
+        precision = Tp / (Tp +Fp)
+        recall = Tp / ( (Tp + Fn))
+        accuracy_scores.append(accuracy)
+        print("Accuracy:", accuracy)
+    max_index = np.argmax(accuracy_scores)
+    y_pred_agoda = models[max_index].predict(preprocessed_X_agoda_test.to_numpy())
+    # Create the output DataFrame with id and cancellation columns
+    output_df = pd.DataFrame({"id": X_agoda_test.loc[preprocessed_X_agoda_test.index]['h_booking_id'], "cancellation": y_pred_agoda[max_index]})
+
+    # Save the output DataFrame to a CSV file
+    output_df.to_csv("agoda_cancellation_prediction.csv", index=False)
 
 if __name__ == '__main__':
     soft_svm_model()
--- preprocess.py
+++ preprocess.py
@@ -1,12 +1,11 @@
 import numpy as np
 import pandas as pd
 from pandas import DataFrame
-
+PERCENTAGE_FACTOR = 100
 
 # Idan and Gal function
 def preprocess_lines_training(df: DataFrame):
     df = df.drop_duplicates()
-
     for c in ['request_latecheckin', 'request_highfloor', 'request_largebed', 'request_twinbeds',
               'request_airport', 'request_earlycheckin']:
         df[c] = df[c].fillna(0)
@@ -20,7 +19,6 @@ def preprocess_lines_training(df: DataFrame):
     df['accommadation_type_name'] = df['accommadation_type_name'].fillna(-1)
     df['accommadation_type_name'] = pd.factorize(df['accommadation_type_name'])[0]
 
-
     for c in ["no_of_adults", 'no_of_room', ]:
         df = df[df[c] > 0]
     for c in ["no_of_children", 'original_selling_amount', ]:
@@ -30,19 +28,40 @@ def preprocess_lines_training(df: DataFrame):
     return df
 
 
-def preprocess_train(X_train: DataFrame, y_train: DataFrame):
+def preprocess_lines_testing(df: DataFrame):
+    df = df.drop_duplicates()
+    for c in ['request_latecheckin', 'request_highfloor', 'request_largebed', 'request_twinbeds',
+              'request_airport', 'request_earlycheckin']:
+        df[c] = df[c].fillna(0)
+        df[c] = df[c].astype(int)
+        if not set(df[c]).issubset({0, 1}):
+            df.loc[~df[c].isin([0, 1]), c] = 0
+
+    df['hotel_brand_code'] = df['hotel_brand_code'].fillna(-1)
+    df['accommadation_type_name'] = df['accommadation_type_name'].fillna(-1)
+    df['accommadation_type_name'] = pd.factorize(df['accommadation_type_name'])[0]
+
+    for c in ["no_of_adults", 'no_of_room', ]:
+        df[c] = df[c].where(df[c] > 0, other=get_average(df[c]))
+    for c in ["no_of_children", 'original_selling_amount', ]:
+        df[c] = df[c].where(df[c] >= 0, other=get_average(df[c]))
+    df["hotel_star_rating"] = df["hotel_star_rating"].fillna(0).astype(float)
+    df[c] = df[c].where((df[c] < 6) & (df[c] >= 0), other=get_average(df[c]))
+    return df
+
+
+def preprocess_test(X_test: DataFrame):
     """
 
     :param filename:
     :return:
     """
-    drop_for_now = ['guest_nationality_country_name','hotel_live_date','original_payment_method']
+    drop_for_now = ['guest_nationality_country_name', 'hotel_live_date', 'original_payment_method']
 
     to_drop = ['hotel_chain_code', 'hotel_area_code', 'request_nonesmoke',
                'original_payment_currency', 'language', 'origin_country_code', 'no_of_extra_bed',
                'customer_nationality', 'h_customer_id']
-    X_train.insert(loc=len(X_train.columns), column="cancellation_datetime", value=y_train)
-    df = preprocess_lines_training(X_train)
+    df = preprocess_lines_testing(X_test)
     df = df.drop(to_drop + drop_for_now, axis=1)
 
     to_date_time = ['booking_datetime', 'checkin_date', 'checkout_date', 'cancellation_datetime']
@@ -55,7 +74,7 @@ def preprocess_train(X_train: DataFrame, y_train: DataFrame):
     df = df.drop(to_date_time, axis=1)
 
     df = pd.get_dummies(df, prefix='in_country_', columns=['hotel_country_code'])
-    df = pd.get_dummies(df,prefix='cancellation_policy_', columns=['cancellation_policy_code'])
+    df = pd.get_dummies(df, prefix='cancellation_policy_', columns=['cancellation_policy_code'])
 
     df = pd.get_dummies(df, prefix='payment_type_', columns=['original_payment_type'])
     df['charge_option'] = df['charge_option'].replace({'Pay Now': 1, 'Pay Later': 0, 'Pay at Check-in': 0})
@@ -64,6 +83,69 @@ def preprocess_train(X_train: DataFrame, y_train: DataFrame):
     return df, y
 
 
+def preprocess(X: DataFrame, y: DataFrame):
+    """
+
+    :param filename:
+    :return:
+    """
+    new_y = None
+    global dummies_hotel_country_code
+    global dummis_cancellation_policy_code
+    global dummis_original_payment_type
+    global global_train_X
+    if y is not None:  # In train
+        X.insert(loc=len(X.columns), column="cancellation_datetime", value=y)
+        X = preprocess_lines_training(X)
+        new_y = X['cancellation_datetime'].notnull().astype(int)
+        X = X.drop(columns=['cancellation_datetime'])
+    else:  # In test
+        X = preprocess_lines_testing(X)
+    X = pd.get_dummies(X, prefix='in_country_', columns=['hotel_country_code'])
+    X = pd.get_dummies(X, prefix='cancellation_policy_', columns=['cancellation_policy_code'])
+    X = pd.get_dummies(X, prefix='payment_type_', columns=['original_payment_type'])
+    drop_for_now = ['guest_nationality_country_name', 'hotel_live_date', 'original_payment_method']
+    to_drop = ['hotel_id', 'hotel_brand_code', 'hotel_city_code', 'hotel_chain_code', 'hotel_area_code',
+               'request_nonesmoke',
+               'original_payment_currency', 'language', 'origin_country_code', 'no_of_extra_bed',
+               'customer_nationality', 'h_customer_id']
+    X = X.drop(to_drop + drop_for_now, axis=1)
+    to_date_time = ['booking_datetime', 'checkin_date', 'checkout_date']
+
+    X[to_date_time] = X[to_date_time].apply(pd.to_datetime)
+
+
+
+    X['time_reserve_before_checking'] = (X['checkin_date'] - X['booking_datetime']).dt.days
+    X['num_of_day_to_stay'] = (X['checkout_date'] - X['checkin_date']).dt.days
+    X = X.drop(to_date_time, axis=1)
+    X['charge_option'] = X['charge_option'].replace({'Pay Now': 1, 'Pay Later': 0, 'Pay at Check-in': 0})
+    X['is_user_logged_in'] = X['is_user_logged_in'].astype(int)
+    X['is_first_booking'] = X['is_first_booking'].astype(int)
+    return X, new_y
+
+
 def get_average(vec: np.ndarray):
     return np.nanmean(vec)
 
+
+def split_policy_code(s: str, total_day: int):
+    days = set()
+    if s == 'UNKNOWN':
+        return [(0, 0)], set()
+
+    splited_polcis = s.split('_')
+    res = []
+    for s in splited_polcis:
+        index_D = s.find('D')
+        num_days = int(s[:index_D])
+        days.add(num_days)
+        percent, percent_id = 0, s.find('P')
+        if percent_id != -1:
+            percent = int(s[index_D + 1:percent_id]) / PERCENTAGE_FACTOR
+        else:
+            percent_id = s.find('N')
+            percent = int(s[index_D + 1:percent_id]) / total_day
+        res.append((num_days, percent))
+
+    return res, days
